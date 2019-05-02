c92a39084cc465f74a7408bb8f94f6c075031392
Arnaud Wetzel <arnaud.wetzel@kbrwadventure.com>
2019-05-02 19:54:16 +0200

    update package metadata and docs deps

--- mix.exs
+++ mix.exs
@@ -7,7 +7,20 @@ defmodule Dotx.MixProject do
       version: "0.2.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
-      deps: []
+      name: "Dotx Elixir dot parser",
+      package: [
+        description: """
+          Dotx is a full feature library for DOT file parsing and generation.
+          The whole spec [https://www.graphviz.org/doc/info/lang.html](https://www.graphviz.org/doc/info/lang.html) is implemented.
+          """,
+        links: %{repo: "https://github.com/kbrw/dotx", doc: "https://hexdocs.pm/dotx"},
+        licenses: ["MIT"],
+      ],
+      source_url: "https://github.com/kbrw/dotx",
+      docs: [main: "Dotx"],
+      deps: [
+        {:ex_doc, "~> 0.19", only: :dev, runtime: false},
+      ]
     ]
   end
 
